b75b9ac4f0e069ae5651d27413ba899effd68dfa
AcsBianka <aacsbianka@gmail.com>
2021-09-20 22:18:24 +0200

    2. ora hazi feladat

--- hf2_1.py
+++ hf2_1.py
@@ -1,8 +1,5 @@
-#1.feladat
-
 r = 4 #cm
 pi = 3.14
 V = (4*pi*r**3)/3
 print("Egy 5 cm sugarú gömb térfogata: ", V , " cm^3.")
 
-#2.feladat
---
+++ hf2_2.py
@@ -0,0 +1,4 @@
+coordinate_1 = 25.14
+coordinate_2 = 54.23
+distance = coordinate_1-coordinate_2
+print(distance)
